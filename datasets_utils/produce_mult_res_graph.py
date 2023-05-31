45d85e4d9a226b454e6639eebf29871e7b59bb92
rz0012 <rz0012@mix.wvu.edu>
2023-05-30 21:56:44 -0400

    minor

--- datasets_utils/produce_mult_res_graph.py
+++ datasets_utils/produce_mult_res_graph.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.legend_handler import HandlerTuple
 import seaborn
-seaborn.set()
+# seaborn.set()
 
 
 path = '/mnt/12F9CADD61CB0337/results/cell_analysis/new_cellTranspose_morphology_loss_pretrained_adaptation_results/Adaptation/TissueNet/3-shot'
@@ -46,12 +46,14 @@ cmap2 = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.Reds)
 
 plots = []
 legends = []
+plt.text(0.75,0.9 ,'CellTranspose', fontsize = 10, color=cmap1.to_rgba(14))
+plt.text(0.9,0.9 ,'Cellpose', fontsize = 10, color=cmap2.to_rgba(14))
 for i in range(len(AP)):
     a, = plt.plot(tau, AP[i], color=cmap1.to_rgba(i+1), marker='', fillstyle='left')
     b, = plt.plot(tau, AP[i], color=cmap2.to_rgba(i+1), marker='', fillstyle='right')
     plots.append((a,b))
     legends.append(os.path.splitext(list(sorted_AP.keys())[i])[0])
-
+    
 plt.legend((plots), (legends), numpoints=1, fontsize=9,  labelspacing=-2, 
            frameon=False, loc='center left', handler_map={tuple: HandlerTuple(ndivide=None)}, handlelength=3)
 plt.savefig('AP Results')
